e839ed2ab0e62982abe32b79e9c55cda17261bcb
1326066792 <165539972+1326066792@users.noreply.github.com>
2026-01-06 19:52:33 -0600

    first website

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <div class="container">
+        <h1>你好，我是 Chance</h1>
+
+        <p class="subtitle">一名正在学习计算机的学生</p>
+
+        <p>
+        这是我从零开始写的第一个个人网站，以后会不断升级。
+        </p>
+        </div>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
